502a1bfed5fe694f4fed626ce1bee52cd804e30d
aldi trainatama <150660869+OrdinaryGUY25-2210@users.noreply.github.com>
2026-09-25 03:06:33 +0800

    Add files via upload

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Sidebar } from '@/components/sidebar/Sidebar';
-import { Navbar } from './Navbar';
+"use client";
+
+import React from "react";
+import Sidebar from "@/components/sidebar/Sidebar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-export function DashboardLayout({ children }: DashboardLayoutProps) {
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex min-h-screen bg-zinc-50 dark:bg-zinc-950">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <Navbar />
-        <main className="flex-1 overflow-y-auto p-6">
-          {children}
-        </main>
+        <main className="flex-1 p-6">{children}</main>
       </div>
     </div>
   );
